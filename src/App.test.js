c799cbda0114ea1dc990fb1ffadde8be405299c8
Chris McC <mcclelland.c@gmail.com>
2019-02-06 18:02:23 +0000

    Upgrade to latest React, getting off alpha.

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import App from './App';
 import { render, cleanup, waitForElement } from 'react-testing-library';
+// TODO: resolve act warning https://github.com/kentcdodds/react-testing-library/issues/281
+// import { act } from 'react-dom/test-utils';
+
 afterEach(cleanup);
 
 test('renders launches', async () => {
