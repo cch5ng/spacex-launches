5f74915028563954c96b56338bd3aed57eb6326d
Carol Chung <cch5ng@gmail.com>
2019-02-10 09:27:45 -0800

    add functions to sort launches by date; sortByDate(), merge(), mergeSort()

--- src/helpers.js
+++ src/helpers.js
@@ -33,3 +33,68 @@ export async function commentsRetrieve(launchId) {
   const commentsForLaunch = data.launchCommentsByFlightNumber.items;
   return commentsForLaunch;
 }
+
+export function merge(left, right) {
+	let mergedAr = [];
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			mergedAr.push(left.shift())
+		} else {
+			mergedAr.push(right.shift())
+		}
+	}
+
+	if (left.length) {
+		mergedAr = mergedAr.concat(left)
+	}
+	if (right.length) {
+		mergedAr = mergedAr.concat(right)
+	}
+	return mergedAr
+}
+
+export function mergeSort(arr) {
+	if (arr.length === 1) {
+		return arr;
+	}
+
+	// simple case is [1, 2]
+	let mid = Math.floor(arr.length / 2);
+	let left = arr.slice(0, mid);
+	let right = arr.slice(mid);
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+export function sortByDate(objAr) {
+	let launchKeyedDateStr = {};
+	let dateKeyedDateMs = {};
+	let sortedDateMsAr = [];
+	let objArSortedByDate = [];
+
+	// build launchKeyedDateStr & dateKeyedDateMs
+	objAr.forEach(obj => {
+		let dateStr = obj.launch_date_utc;
+		launchKeyedDateStr[dateStr] = obj;
+
+		let dateObj = new Date(dateStr);
+		let dateMs = Date.UTC(dateObj.getUTCFullYear(),
+			dateObj.getUTCMonth(),
+			dateObj.getUTCDate(),
+			dateObj.getUTCHours(),
+			dateObj.getUTCMinutes(),
+			dateObj.getUTCSeconds()
+		);
+
+		dateKeyedDateMs[dateMs] = dateStr;
+	});
+
+	sortedDateMsAr = mergeSort(Object.keys(dateKeyedDateMs));
+	objArSortedByDate = sortedDateMsAr.map((dateMs2, idx) => {
+		let dateStr2 = dateKeyedDateMs[dateMs2];
+	
+		return launchKeyedDateStr[dateStr2];
+	});
+
+	return objArSortedByDate;
+}
+
